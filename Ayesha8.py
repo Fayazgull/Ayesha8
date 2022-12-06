cc0396ce00434804e81da257dbc20555f9ffe2c8
Fayazgull <117324728+Fayazgull@users.noreply.github.com>
2022-12-06 08:10:42 +0500

    Ayesha8.py
    pkg update
    pkg upgrade
    pkg install python
    pkg install python2
    pip install requests
    pip install mechanize
    pip install bs4
    pkg install git
    pip install lolcat
    git clone https://github.com/Fayazgull/Ayesha8.git
    ls
    cd Ayesha8
    ls
    python Ayesha8.py

--- Ayesha8.py
+++ Ayesha8.py
@@ -246,7 +246,7 @@ def dynamic(text):
         sys.stdout.flush();time.sleep(1) 
  
 #User agents
-ugen2=['Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36 [FBAN/EMA;FBLC/id_ID;FBAV/239.0.0.10.109;]']
+ugen2=['Mozilla/5.0 (Linux; Android 13) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.79 Mobile Safari/537.36
 ugen=['Mozilla/5.0 (Linux; Android 5.0; ASUS_Z00AD Build/LRX21V) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/37.0.0.0 Mobile Safari/537.36 [FBAN/EMA;FBLC/id_ID;FBAV/239.0.0.10.109;]']
  
 for xd in range(10000):
